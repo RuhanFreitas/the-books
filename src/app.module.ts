239743ce0da132c9a85b21c16dce0925ba763f5a
Ruhan Freitas <jspruhanfreitas@gmail.com>
2026-01-27 16:05:15 -0300

    generate admin and review resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AdminController } from './admin/admin.controller';
+import { AdminModule } from './admin/admin.module';
+import { ReviewModule } from './review/review.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AdminModule, ReviewModule],
+  controllers: [AdminController],
+  providers: [],
 })
 export class AppModule {}
