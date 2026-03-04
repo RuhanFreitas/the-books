33194cc073616cb0acf37dc0fed4d28243c8be47
Ruhan Freitas <jspruhanfreitas@gmail.com>
2026-03-04 15:01:11 -0300

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ConsoleLogger, ValidationPipe } from '@nestjs/common'
 import { LoggingInterceptor } from './common/interceptors/loggin.interceptor'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
@@ -13,6 +14,14 @@ async function bootstrap() {
             whitelist: true,
         })
     )
+
+    const config = new DocumentBuilder()
+    .setTitle('The Books')
+    .setVersion('1.0')
+    .build()
+
+    const documentFactory = () => SwaggerModule.createDocument(app, config)
+    SwaggerModule.setup('api', app, documentFactory)
 
     await app.listen(process.env.PORT ?? 3000)
 }
